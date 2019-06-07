7199e65f1465e0f0ef5fec56acf75092dac34da8
alenaaleyn1kova <48382053+alenaaleyn1kova@users.noreply.github.com>
2019-06-07 23:12:56 +0500

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace AstronomicalCatalog
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            var ofd = new OpenFileDialog() { Filter = "Фотография|*.jpg" };
+            var dr = ofd.ShowDialog(this);
+            if (dr == DialogResult.OK)
+            {
+                pictureBox1.Image = Image.FromFile(ofd.FileName);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var ofd = new OpenFileDialog() { Filter = "Фотография|*.jpg" };
+            var dr = ofd.ShowDialog(this);
+            if (dr == DialogResult.OK)
+            {
+                pictureBox1.Image = Image.FromFile(ofd.FileName);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem is Star)
+            {
+                listBox1.Items.Remove(listBox1.SelectedItem);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var add = new Data() { Data_Planets = new Star() };
+            if (add.ShowDialog(this) == DialogResult.OK)
+            {
+                listBox1.Items.Add(add.Data_Planets);
+            }
+        }
+    }
+}
